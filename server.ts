e0c73c1e7928ba7ee012d0ff151af945aa674cee
Fadi Hanna <54924938+fadihanna123@users.noreply.github.com>
2021-03-08 03:17:50 +0100

    Remove auth code

--- server.ts
+++ server.ts
@@ -1,25 +1,8 @@
-import { NextFunction, Request as ExpressRequest, Response } from "express";
-import jwt from "jsonwebtoken";
-
-import { IUsers, Request } from "./typings/List";
 import { server } from "./config/GlobalSettings";
 import "./config/database";
 import "./config/settings";
 import "./api/AuthRoutes";
 import UserRoutes from "./api/UserRoutes";
-
-const auth = async (req: Request, res: Response, next: NextFunction) => {
-  const Header = req.headers.authorization;
-  const token = Header && Header.split(" ")?.[1];
-  if (!token) return res.sendStatus(401);
-  try {
-    const user = jwt.verify(token, "X");
-    req.user = user as IUsers;
-    next();
-  } catch (err) {
-    return res.sendStatus(403);
-  }
-};
 
 server.use(UserRoutes);
 
